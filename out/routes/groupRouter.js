a3b01b0b031ceaa3ed59452e084cdaa5fd29f756
Yacine Ouardi <ouardi.yacin3@gmail.com>
2023-02-11 23:22:24 +0100

    vercel image

--- out/routes/groupRouter.js
+++ out/routes/groupRouter.js
@@ -6,16 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.groupRouter = void 0;
 const express_1 = require("express");
 const multer_1 = __importDefault(require("multer"));
+const path_1 = __importDefault(require("path"));
 const groupController_1 = require("../controllers/groupController");
 const authMiddlware_1 = require("../middlwares/authMiddlware");
 const groupRouter = (0, express_1.Router)();
 exports.groupRouter = groupRouter;
 const storage = multer_1.default.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public");
+        cb(null, path_1.default.join(__dirname, "public/images"));
     },
     filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`);
+        cb(null, file.fieldname + "-" + Date.now() + path_1.default.extname(file.originalname));
     },
 });
 const upload = (0, multer_1.default)({ storage });
